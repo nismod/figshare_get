700910bd4256b54e8f26665b0ca45c68c79acc63
Tom Russell <tom.russell@ouce.ox.ac.uk>
2025-07-24 15:23:53 +0100

    Drop text search, use DOI, allow either article or collection

--- src/figshare_get/cli.py
+++ src/figshare_get/cli.py
@@ -9,6 +9,7 @@ all files in each article.
 Then it uses the "download_url" and "name" of each file to download and save to
 the working directory.
 """
+
 import argparse
 import json
 import os
@@ -20,49 +21,55 @@ import requests
 def figshare_get(args=None):
     parser = argparse.ArgumentParser(prog="figshare_get")
     parser.add_argument(
-        "search",
+        "doi",
         type=str,
-        help="Search using a DOI or description of a figshare collection to download",
-    )
-    parser.add_argument(
-        "-n",
-        "--result-number",
-        required=False,
-        type=int,
-        help="Download result number (0-9) without prompting",
+        help="Search using the DOI of a figshare collection or article to download",
     )
     args = parser.parse_args(args)
 
-    results = search(args.search)
+    # search articles
+    articles = search_articles(args.doi)
+    result = check_results(articles)
+
+    # if found, download then quit
+    if result is not None:
+        id_ = result["id"]
+        print("Downloading from", result["doi"], result["title"])
+        download_article_meta(id_)
+        download_article_files(id_)
+        sys.exit()
+
+    # else search collections
+    collections = search_collections(args.doi)
+    result = check_results(collections)
+
+    if result is None:
+        print("Not found")
+        sys.exit()
+
+    id_ = result["id"]
+    print("Downloading from", result["doi"], result["title"])
+    download_collection_meta(id_)
+    download_collection_files(id_)
+
+
+def check_results(results):
     if "message" in results:
         print(results)
         sys.exit()
     elif len(results):
-        if args.result_number is not None:
-            result = results[args.result_number]
-        else:
-            for i, result in enumerate(results):
-                print("  ", i, result["title"], result["doi"])
-            result_num_str = input(
-                "Type the number of a result to download, or ENTER to skip\n"
-            )
-            if not len(result_num_str):
-                sys.exit()
-            else:
-                try:
-                    result_num = int(result_num_str)
-                    result = results[result_num]
-                except:
-                    print("Did not provide a number in range")
-                    sys.exit()
-
+        result = results[0]
     else:
-        print("No results")
-        sys.exit()
+        result = None
+    return result
 
-    collection_id = result["id"]
-    download_collection_meta(collection_id)
-    download_collection_files(collection_id)
+
+def download_article_meta(article_id):
+    meta = get_article(article_id)
+    fname = f"metadata_{article_id}.json"
+    print("Saving metadata to", fname)
+    with open(fname, "w") as fd:
+        json.dump(meta, fd, indent=2)
 
 
 def download_collection_meta(collection_id):
@@ -71,6 +78,16 @@ def download_collection_meta(collection_id):
     print("Saving metadata to", fname)
     with open(fname, "w") as fd:
         json.dump(meta, fd, indent=2)
+
+
+def download_article_files(article_id):
+    for file_meta in get_article_files(article_id):
+        fname = file_meta["name"]
+        if os.path.exists(fname):
+            print("Skipping", fname)
+        else:
+            print("Downloading", fname)
+            save_file(file_meta)
 
 
 def download_collection_files(collection_id):
@@ -91,15 +108,29 @@ def get_collection_files(collection_id):
             yield file_meta
 
 
-def search(string):
+def search_articles(string):
+    url = "https://api.figshare.com/v2/articles/search"
+    data = {"doi": string}
+    r = requests.post(url, data=json.dumps(data))
+    return r.json()
+
+
+def search_collections(string):
     url = "https://api.figshare.com/v2/collections/search"
-    data = {"search_for": string}
+    data = {"doi": string}
     r = requests.post(url, data=json.dumps(data))
     return r.json()
 
 
 def get_collection(collection_id):
     url = f"https://api.figshare.com/v2/collections/{collection_id}"
+    r = requests.get(url)
+    data = r.json()
+    return data
+
+
+def get_article(article_id):
+    url = f"https://api.figshare.com/v2/articles/{article_id}"
     r = requests.get(url)
     data = r.json()
     return data
